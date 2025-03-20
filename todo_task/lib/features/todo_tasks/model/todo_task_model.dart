6cd3d1fef3dc54ad31493e5b1ab58b97fbc09fb5
Alexandre Marques Bulcao <alexandre.bulcao@einstein.br>
2025-03-20 17:00:59 -0400

    Begin project

---
+++ todo_task/lib/features/todo_tasks/model/todo_task_model.dart
@@ -0,0 +1,8 @@
+class TodoTaskModel {
+  String? title;
+  String? description;
+  String? dueDate;
+  String? priority;
+  String? createdByEmail;
+  String? createdByName;
+}
---
+++ todo_task/lib/features/todo_tasks/view/todo_task_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TodoTaskView extends StatefulWidget {
+  const TodoTaskView({ Key? key }) : super(key: key);
+
+  @override
+  _TodoTaskViewState createState() => _TodoTaskViewState();
+}
+
+class _TodoTaskViewState extends State<TodoTaskView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
