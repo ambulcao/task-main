fb43bc58589c41197ae154ae6e5ddd4f57ce85e6
Alexandre Marques Bulcao <alexandre.bulcao@einstein.br>
2025-03-20 21:27:38 -0400

    Add model task todo

--- lib/features/todo_tasks/model/todo_task_model.dart
+++ lib/features/todo_tasks/model/todo_task_model.dart
@@ -3,6 +3,41 @@ class TodoTaskModel {
   String? description;
   String? dueDate;
   String? priority;
+  CreatedBy? createdBy;
+
+  TodoTaskModel({this.title, this.description, this.dueDate, this.priority, this.createdBy});
+  TodoTaskModel.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    description = json['description'];
+    dueDate = json['dueDate'];
+    priority = json['priority'];
+    createdBy = json['createdBy' != null ? CreatedBy.fromJson(json['createdBy']) : null ];
+  }
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['title'] = title;
+    data['description'] = description;
+    data['dueDate'] = dueDate;
+    data['priority'] = priority;
+    if (createdBy != null) {
+      data['createdBy'] = createdBy!.toJson();
+    }
+    return data;
+  }
+}
+
+class CreatedBy {
   String? createdByEmail;
   String? createdByName;
+  CreatedBy({this.createdByEmail, this.createdByName});
+  CreatedBy.fromJson(Map<String, dynamic> json) {
+    createdByEmail = json['email'];
+    createdByName = json['name'];
+  }
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['email'] = createdByEmail;
+    data['name'] = createdByName;
+    return data;
+  }
 }
